242951feda714f31c082beff4d5bde8b6d695ed7
oiibar <95119839+oiibar@users.noreply.github.com>
2024-05-28 15:32:34 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://cat-wiki-cli.onrender.com"],
     credentials: true,
     optionsSuccessStatus: 200,
   })

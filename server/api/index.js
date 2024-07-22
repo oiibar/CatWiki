59ecd2467e9d520f9759f014a650f989a3f6df4c
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-07-23 01:05:12 +0500

    deploy

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,2 @@
-import express from "express";
-import cors from "cors";
-import breedRoutes from "../routes/breeds.router.js";
-
-const app = express();
-app.use(express.json());
-//{origin: ["https://cat-wiki-cli.vercel.app"]}
-app.use(cors());
-
-// Example route
-app.use("/api/breeds", breedRoutes);
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+import app from "../index.js";
+export default app;

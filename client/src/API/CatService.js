f87b825c2375ae8d40eabdd47abb04e5f1f06a95
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-07-23 10:09:47 +0500

    deploy

--- client/src/API/CatService.js
+++ client/src/API/CatService.js
@@ -4,39 +4,35 @@ const api = axios.create({
   timeout: 5000,
 });
 
-class CatService {
-  static async getBreeds() {
-    try {
-      const response = await api.get(`${BASE_URL}`);
-      return response.data;
-    } catch (e) {
-      console.error("Error fetching breeds:", e.message);
-      return { error: e.message };
-    }
+export const getBreeds = async () => {
+  try {
+    const response = await api.get(`${BASE_URL}`);
+    return response.data;
+  } catch (e) {
+    console.error("Error fetching breeds:", e.message);
+    return { error: e.message };
   }
+};
 
-  static async getBreedById(breedId) {
-    try {
-      const response = await api.get(`${BASE_URL}/${breedId}`);
-      return response.data;
-    } catch (e) {
-      console.error(`Error fetching breed with ID ${breedId}:`, e.message);
-      return { error: e.message };
-    }
+export const getBreedById = async (breedId) => {
+  try {
+    const response = await api.get(`${BASE_URL}/${breedId}`);
+    return response.data;
+  } catch (e) {
+    console.error(`Error fetching breed with ID ${breedId}:`, e.message);
+    return { error: e.message };
   }
+};
 
-  static async getImagesByBreedId(breedId) {
-    try {
-      const response = await api.get(`${BASE_URL}/images/${breedId}`);
-      return response.data;
-    } catch (e) {
-      console.error(
-        `Error fetching images for breed with ID ${breedId}:`,
-        e.message
-      );
-      return { error: e.message };
-    }
+export const getImagesByBreedId = async (breedId) => {
+  try {
+    const response = await api.get(`${BASE_URL}/images/${breedId}`);
+    return response.data;
+  } catch (e) {
+    console.error(
+      `Error fetching images for breed with ID ${breedId}:`,
+      e.message
+    );
+    return { error: e.message };
   }
-}
-
-export default CatService;
+};
